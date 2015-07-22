5cafb883fe5d6b78fde30e1b5fee9e3d38737ff9
Jorge Schrauwen <sjorge@blackdot.be>
2015-07-22 16:22:35 +0000

    bundle asmd_* libraries

--- setup.py
+++ setup.py
@@ -4,11 +4,25 @@
 # ----------------------------------
 # esky + bbfreeze setup script
 #
+import sys, os
 from esky import bdist_esky
 from distutils.core import setup
+sys.path.append(sys.path[0] + '/modules')
 
-setup(name="asmd",
-      version="1.0.0",
-      scripts=["asmd.py","mods/config.py"],
-      options={"bdist_esky":{"includes":[]}},
-     )
+modules = []
+for f in os.listdir(sys.path[0]):
+  if not os.path.isfile(os.path.join(sys.path[0], 'modules', f)):
+    continue
+  if f.startswith('asmd_') and f.endswith('.py'):
+    modules.append(f[:-3])
+
+setup(
+  name = "asmd",
+  version = "1.0.0",
+  scripts = [
+        "asmd.py",
+  ],
+  options = { "bdist_esky": {
+    "includes": modules
+  }},
+)
