4bfa25aaceae5a1340ab55bc0a7b19be2bf16a50
Jorge Schrauwen <sjorge@blackdot.be>
2015-07-22 19:34:07 +0000

    asmd :: load and call service.start/service.stop

--- asmd.py
+++ asmd.py
@@ -22,15 +22,29 @@ class asmd(object):
     module_object = getattr(__import__('asmd_config'), 'asmd_config')
     (module_object()).run()
 
+  def run(self, service, methode):
+    log("loading service %s ..." % service)
+
+    try:
+      module_object = getattr(__import__('asmd_service_%s' % service), 'asmd_service_%s' % service)
+      if methode.lower() == 'start':
+        (module_object()).start()
+      if methode.lower() == 'stop':
+        (module_object()).stop()
+    except:
+      log("failed to load serivce %s!" % service)
+
+
 ## init
 if __name__ == "__main__":
   """initialized asmd based on argv"""
   if '-s' in sys.argv:
     # setup mode, ignore other opts
     asmd().setup()
-  elif '-d' in sys.argv:
-    # service mode, parse next opt
-    pass
+  elif '-d' in sys.argv and '-m' in sys.argv:
+    service = sys.argv[(1+sys.argv.index('-d'))]
+    methode = sys.argv[(1+sys.argv.index('-m'))]
+    asmd().run(service, methode)
   else:
     log('please run asmd via smf or provide -s to run setup.', error=True)
     sys.exit(1)
