60ecda4a27144705e07572513457b001e31ccca6
Jorge Schrauwen <sjorge@blackdot.be>
2015-07-22 19:58:39 +0000

    rename shell service to profile service

---
+++ data/modules/asmd_service_profile.py
@@ -0,0 +1,31 @@
+from asmd_logger import log
+import os
+class asmd_service_profile(object):
+  """asmd profile service :: setup /root's home"""
+  profile_config_dir = "/usbkey/asmd/profile"
+
+  def __init__(self):
+    """pre-launch stuff for profile service"""
+    log("initializing ...", log_name='asmd::service::profile')
+
+    if not os.path.isdir(self.profile_config_dir):
+      log("creating data directory %s ..." % self.profile_config_dir, log_name='asmd::service::profile')
+      os.makedirs(self.profile_config_dir)
+
+  def start(self):
+    log("starting ...", log_name='asmd::service::profile')
+    # TODO: implement me
+
+  def stop(self):
+    """daemon stop"""
+    pass # not required for transient service
+
+  def smf_instance_config(self):
+    config = {}
+    config['service'] = "profile-setup"
+    config['transient'] = True
+    config['description'] = "Setup /root's home"
+    config['dependencies'] = {}
+    config['dependencies']['fs-local'] = "svc:/system/filesystem/local"
+   
+    return config
