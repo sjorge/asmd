c6ef1477bd584291f71a8e8c44a7b82a0724ea0a
Jorge Schrauwen <sjorge@blackdot.be>
2015-07-22 15:22:42 +0000

    renamed library directories

--- mods/config.py
+++
@@ -1,1 +0,0 @@
-## TODO: implement config parser
---
+++ modules/asmd_modules_config.py
@@ -0,0 +1,5 @@
+class asmd_modules_config(object):
+  """asmd configuration helper"""
+
+  def run(self):
+    asmd_helper().log("configuration running!")
