d4370a55a8f18e157935e34095486fa0dd725f31
Jorge Schrauwen <sjorge@blackdot.be>
2015-07-23 23:20:47 +0000

    include additional bash helpers

---
+++ share/bash_helpers/update_hosts.sh
@@ -0,0 +1,14 @@
+update_hosts() {
+  HOST_TEMP=$(mktemp)
+  head -n26 /etc/hosts > ${HOST_TEMP}
+  sleep 5 # give addrconf time to settle
+  for IP in $(ipadm show-addr | grep e1000g0 | awk '{ print $NF }' | awk -F'/' '{ print $1 }'); do
+    asmd_core_log "updating /etc/hosts for ${IP} ..."
+    if [ -z "$(domainname)" ]; then
+      echo "${IP}\t$(hostname)" >> ${HOST_TEMP}
+    else
+      echo -e  "${IP}\t$(hostname).$(domainname) $(hostname)" >> ${HOST_TEMP}
+    fi
+  done
+  mv ${HOST_TEMP} /etc/hosts
+}
