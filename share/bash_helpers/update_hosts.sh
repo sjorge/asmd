3900cd3fd7cbf02f51e6aea6e38afebfecded7a8
Jorge Schrauwen <sjorge@blackdot.be>
2015-07-23 23:26:31 +0000

    bash_helper::update_hosts - fix bad grep for nic

--- share/bash_helpers/update_hosts.sh
+++ share/bash_helpers/update_hosts.sh
@@ -2,7 +2,7 @@ update_hosts() {
   HOST_TEMP=$(mktemp)
   head -n26 /etc/hosts > ${HOST_TEMP}
   sleep 5 # give addrconf time to settle
-  for IP in $(ipadm show-addr | grep e1000g0 | awk '{ print $NF }' | awk -F'/' '{ print $1 }'); do
+  for IP in $(ipadm show-addr | grep -v lo0 | awk '{ print $NF }' | awk -F'/' '{ print $1 }'); do
     asmd_core_log "updating /etc/hosts for ${IP} ..."
     if [ -z "$(domainname)" ]; then
       echo "${IP}\t$(hostname)" >> ${HOST_TEMP}
