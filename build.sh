df47d907547fedcd2a3ae7e82a2840be94101f0e
Jorge Schrauwen <sjorge@blackdot.be>
2015-07-23 12:53:12 +0000

    asmd:build - better bin handling

--- build.sh
+++ build.sh
@@ -6,9 +6,10 @@
 #
 VERSION=1.0.0
 [ -d dist/ ] && rm -rf dist/
-mkdir -p dist/asmd
 mkdir -p dist/asmd/{bin,etc}
-cp asmd.sh dist/asmd/bin/asmd
+cp bin/asmd.sh dist/asmd/bin/asmd
+cp bin/asmd-setup.sh dist/asmd/bin/asmd-setup
+find dist/asmd/bin/ -type f -exec chmod +x {} \;
 cp -r share dist/asmd/
 cp -r services dist/asmd/
 tar czvf dist/asmd-${VERSION}.tar.gz -C dist/ asmd/
