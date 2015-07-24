4e41888fac0d47029d8eeb362a3e524ad75f43ce
Jorge Schrauwen <sjorge@blackdot.be>
2015-07-24 17:17:35 +0000

    asmd::helper - axtra useful function

--- share/helpers/ipv6/validate.sh
+++ share/helpers/ipv6/validate.sh
@@ -53,3 +53,23 @@ validate_ipv6() {
   
   return 0
 }
+
+mask2cidr() {
+    nbits=0
+    IFS=.
+    for dec in $1 ; do
+        case $dec in
+            255) let nbits+=8;;
+            254) let nbits+=7;;
+            252) let nbits+=6;;
+            248) let nbits+=5;;
+            240) let nbits+=4;;
+            224) let nbits+=3;;
+            192) let nbits+=2;;
+            128) let nbits+=1;;
+            0);;
+            *) echo "Error: $dec is not recognised"; exit 1
+        esac
+    done
+    echo "$nbits"
+}
